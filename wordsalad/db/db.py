1db9f4bfae0e7991b95e887fdbf43a677d351427
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-19 15:05:15 +0900

    add comments

--- wordsalad/db/db.py
+++ wordsalad/db/db.py
@@ -12,16 +12,21 @@ from flask.cli import with_appcontext
 
 
 def get_db():
+    # If database connection is not already stored in namespace object `g`
     if 'db' not in g:
+        # Create connection to `DATABASE` specified by app pointed to by `current_app`
+        # and store in `g`
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
+        # Return rows as dictionary objects
         g.db.row_factory = sqlite3.Row
 
     return g.db
 
 
+# If connection exists, close it
 def close_db(e=None):
     db = g.pop('db', None)
 
