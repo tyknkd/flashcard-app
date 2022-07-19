5e6a83fdbfffdedc949ec8abc20819b56ae74720
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-19 11:07:26 +0900

    add comments

--- wordsalad/__init__.py
+++ wordsalad/__init__.py
@@ -13,22 +13,23 @@ def create_app(test_config=None):
     # __name__: name of current Python module
     # instance_relative_config=True: config files in instance folder above /wordsalad/
     app = Flask(__name__, instance_relative_config=True)
+
     # Set default configuration
-    # SECRET_KEY should be made more secure (i.e., random) when deploying
-    # DATABASE: path to SQLite database file 
+    # DATABASE: path to SQLite database file (instance folder)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'wordsalad.sqlite'),
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
+        # Load the instance config, if it exists, when not testing
+        # config.py overrides default configuration (e.g., more secure SECRET_KEY)
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
+        # Load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
+    # Check that the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
