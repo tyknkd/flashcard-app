dcb44bafc3ee87fa1d87c7f11bac083efc694ad9
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-19 16:22:44 +0900

    mv db script

--- wordsalad/__init__.py
+++ wordsalad/__init__.py
@@ -68,7 +68,7 @@ def create_app(test_config=None):
         return render_template('edit.html')
 
     # Initialize database
-    from .dbScripts import db
+    from .import db
     db.init_app(app)
 
     return app
