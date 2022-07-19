ecd682a2aab1cf165b8e936b1547262848c66800
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-19 20:26:16 +0900

    Fixed users typo

--- wordsalad/auth.py
+++ wordsalad/auth.py
@@ -122,7 +122,7 @@ def load_logged_in_user():
     else:
         # Query database for user info
         g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
+            'SELECT * FROM users WHERE id = ?', (user_id,)
         ).fetchone()
 
 @bp.route('/logout')
