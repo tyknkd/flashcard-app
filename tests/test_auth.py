3abaadc3149f49d568f405b278acbd334210703a
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-20 14:57:24 +0900

    Correct parameters

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -16,7 +16,7 @@ def test_register(client, app):
     assert client.get('/auth/register').status_code == 200
     
     # Register valid user data 
-    valid_user = {'name': 'a', 'email': 'address@example.com', 'username': 'a', 'password': 'a'}
+    valid_user = {'name': 'Amos', 'email': 'amos@example.com', 'username': 'ajax', 'password': 'amazing'}
     response = client.post(
         '/auth/register', data=valid_user
     )
@@ -27,20 +27,25 @@ def test_register(client, app):
     # Confirm user now in database 
     with app.app_context():
         assert get_db().execute(
-            "SELECT * FROM user WHERE username = 'a'",
+            "SELECT * FROM users WHERE username = 'ajax'",
         ).fetchone() is not None
      
     # TO DO: Confirm all cols of user data
 
-
-@pytest.mark.parametrize(('username', 'password', 'message'), (
-    ('', '', b'Username is required.'),
-    ('a', '', b'Password is required.'),
+# Repeat test with different arguments
+@pytest.mark.parametrize(('name','email','username', 'password', 'message'), (
+    ('', '', '', '', b'Name is required.'),
+    ('Bilbo', '', '', '', b'Email is required.'),
+    ('Bilbo', 'bilbo@example.com', '', b'Username is required.'),
+    ('Bilbo', 'bilbo@example.com', 'bilbao', '', b'Password is required.'),
     ('test', 'test', b'already registered'),
 ))
-def test_register_validate_input(client, username, password, message):
+def test_register_validate_input(client, name, email, username, password, message):
+    '''
+    Test form validation
+    '''
     response = client.post(
         '/auth/register',
-        data={'username': username, 'password': password}
+        data={'name': name, 'email': email, 'username': username, 'password': password}
     )
     assert message in response.data
