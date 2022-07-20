c6a2df3a065f3d729c146af8d0ce353fb2a63fd9
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-20 15:16:01 +0900

    Added login and logout tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -32,13 +32,13 @@ def test_register(client, app):
      
     # TO DO: Confirm all cols of user data
 
-# Repeat test with different arguments
+# Repeat following test with different arguments
 @pytest.mark.parametrize(('name','email','username', 'password', 'message'), (
     ('', '', '', '', b'Name is required.'),
     ('Bilbo', '', '', '', b'Email is required.'),
     ('Bilbo', 'bilbo@example.com', '', b'Username is required.'),
     ('Bilbo', 'bilbo@example.com', 'bilbao', '', b'Password is required.'),
-    ('test', 'test', b'already registered'),
+    ('test', 'test@test.com', 'test', 'test', b'already registered'),
 ))
 def test_register_validate_input(client, name, email, username, password, message):
     '''
@@ -49,3 +49,46 @@ def test_register_validate_input(client, name, email, username, password, messag
         data={'name': name, 'email': email, 'username': username, 'password': password}
     )
     assert message in response.data
+    
+def test_login(client, auth):
+    '''
+    Test login view
+    '''
+    # Confirm login view renders correctly 
+    assert client.get('/auth/login').status_code == 200
+    
+    # Confirm redirect to home page after valid login
+    response = auth.login()
+    assert response.headers["Location"] == "/"
+
+    # Confirm session has correct user_id & username after login
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['username'] == 'test'
+
+# Repeat following test with different arguments
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', b'Incorrect username.'),
+    ('ajax', 'test', b'Incorrect username.'),
+    ('test', '', b'Incorrect password.'),
+    ('test', 'amazing', b'Incorrect password.'),
+))
+def test_login_validate_input(auth, username, password, message):
+    '''
+    Confirm error message returned for invalid username/password
+    '''
+    response = auth.login(username, password)
+    assert message in response.data
+
+def test_logout(client, auth):
+    '''
+    Test logout
+    '''
+    # Valid login
+    auth.login()
+
+    # Confirm user_id not in session after logout
+    with client:
+        auth.logout()
+        assert 'user_id' not in session
