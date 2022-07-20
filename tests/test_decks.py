50fd17aceff15b0161f9e25180c4a94cafeacda1
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-20 16:06:09 +0900

    Add login required tests

--- tests/test_decks.py
+++ tests/test_decks.py
@@ -28,3 +28,47 @@ def test_decks(client, auth):
     # Confirm deck name and description from tests/data.sql displayed on page
     assert b'Test Title' in response.data
     assert b'This is a test deck description.' in response.data
+
+
+# Repeat following test with different arguments
+@pytest.mark.parametrize('path', (
+    '/create',
+    '/1/edit',
+    '/1/delete',
+))
+def test_login_required(client, path):
+    '''
+    Confirm login required
+    '''
+    response = client.post(path)
+    assert response.headers["Location"] == "/auth/login"
+
+
+def test_owner_required(app, client, auth):
+    # Change the deck owner to another user
+    with app.app_context():
+        db = get_db()
+        db.execute('UPDATE decks SET owner_id = 2 WHERE deck_id = 1')
+        db.commit()
+
+    # Login as test user (user_id=1)
+    auth.login()
+    
+    # Confirm current user cannot modify other user's deck
+    assert client.post('/1/edit').status_code == 403
+    assert client.post('/1/delete').status_code == 403
+    
+    # Confirm current user does not see edit link
+    assert b'href="/1/edit"' not in client.get('/decks').data
+
+# Repeat following test with different arguments
+@pytest.mark.parametrize('path', (
+    '/2/edit',
+    '/2/delete',
+))
+def test_exists_required(client, auth, path):
+    '''
+    Confirm nonexistent deck cannot be edited
+    '''
+    auth.login()
+    assert client.post(path).status_code == 404
