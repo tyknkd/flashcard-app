f38baa54bf1bf5638914a6877381d11ae1bd1fd2
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-20 15:29:35 +0900

    Create test_home.py

---
+++ tests/test_home.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# Home page unit tests 
+# Reference: https://flask.palletsprojects.com/en/2.1.x/tutorial/tests/
+
+import pytest
+from wordsalad.db import get_db
+
+def test_home(client, auth):
+    '''
+    Test home/index page
+    '''
+    # Get response to index/root view w/o logging in
+    response = client.get('/')
+    
+    # Confirm "Log In" and "Register" on page
+    assert b"Log In" in response.data
+    assert b"Register" in response.data
+
+    # Login and get response to index view
+    auth.login()
+    response = client.get('/')
+    
+    # Confirm "Log Out" on page
+    assert b'Log Out' in response.data
+    
+     # Confirm deck name and description from tests/data.sql displayed on page
+    assert b'Test Title' in response.data
+    assert b'This is a test deck description.' in response.data
