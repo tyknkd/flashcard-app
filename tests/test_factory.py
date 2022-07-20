592c60593af904f33285ddf7be510f4299e29c4f
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-20 13:06:09 +0900

    Fix typo

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -3,7 +3,7 @@
 # Application factory unit test 
 # Reference: https://flask.palletsprojects.com/en/2.1.x/tutorial/tests/
 
-from flaskr import create_app
+from wordsalad import create_app
 
 def test_config():
     '''
