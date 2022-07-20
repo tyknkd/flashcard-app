b3bd7576632390d1b242120ca9c781603a0b093e
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-20 12:17:22 +0900

    Add comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,33 +10,49 @@ import pytest
 from wordsalad import create_app
 from wordsalad.db import get_db, init_db
 
+# Read SQL test data
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf8')
 
 @pytest.fixture
 def app():
+    '''
+    App generator
+    '''
+    # Create temporary file
     db_fd, db_path = tempfile.mkstemp()
 
+    # Set app to test mode
+    # Override default database path
     app = create_app({
         'TESTING': True,
         'DATABASE': db_path,
     })
 
+    # Initialize database
     with app.app_context():
         init_db()
         get_db().executescript(_data_sql)
 
+    # Return app to caller without terminating this function
     yield app
 
+    # Close and remove temporary file
     os.close(db_fd)
     os.unlink(db_path)
 
 @pytest.fixture
 def client(app):
+    '''
+    Client to handle test requests without server
+    '''
     return app.test_client()
 
 @pytest.fixture
 def runner(app):
+    '''
+    Runner to call Click commands within app
+    '''
     return app.test_cli_runner()
 
 # Class to handle user authentication procedures
