7fa6d4d7ac58f1d0a435854b6c994882b14e73a9
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-20 11:12:56 +0900

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,43 @@
 #!/usr/bin/python3
 
-# Test configuration script to be run for each unit test 
+# Unit test configuration script to be run for each unit test 
 # Reference: https://flask.palletsprojects.com/en/2.1.x/tutorial/tests/
+
+import os
+import tempfile
+
+import pytest
+from wordsalad import create_app
+from wordsalad.db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
 
 # Class to handle user authentication procedures
 class AuthActions(object):
