74ef7d5c1325a81a3fe56fc8c9e107264c6bac8e
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-21 19:50:31 +0900

    changed boolean type

--- wordsalad/schema.sql
+++ wordsalad/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE decks (
   title VARCHAR,
   category VARCHAR,
   description VARCHAR,
-  'public' BOOL, 
+  public BOOLEAN, 
   FOREIGN KEY (owner_id) 
     REFERENCES users (user_id)
 );
