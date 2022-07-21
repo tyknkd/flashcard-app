1c16eba381663bd638cbd7a5f34222f5ddf713e0
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-21 18:08:58 +0900

    Change to 3 tables

--- wordsalad/schema.sql
+++ wordsalad/schema.sql
@@ -1,36 +1,27 @@
-CREATE TABLE `flashcards` (
-  `card_id` INTEGER PRIMARY KEY,
-  `category` varchar(255),
-  `front` varchar(255), 
-  `back` varchar(255), 
-  `notes` varchar(255)
+CREATE TABLE users (
+  user_id INT PRIMARY KEY,
+  name VARCHAR,
+  email VARCHAR,
+  username VARCHAR,
+  password VARCHAR
 );
 
-CREATE TABLE `decks` (
-  `deck_id` INTEGER PRIMARY KEY,
-  `name` varchar(255),
-  `category` varchar(255),
-  `owner_id` int,
-  `public` bool,
-  `description` varchar(255)
+CREATE TABLE decks (
+  deck_id INT PRIMARY KEY,
+  owner_id INT, 
+  title VARCHAR,
+  category VARCHAR,
+  description VARCHAR,
+  public BOOL, 
+  FOREIGN KEY (owner_id) INT 
+    REFERENCES users (user_id)
 );
 
-CREATE TABLE `users` (
-  `user_id` INTEGER PRIMARY KEY,
-  `name` varchar(255),
-  `username` varchar(255),
-  `email` varchar(255),
-  `password` varchar(255)
-);
-
-CREATE TABLE `cards_in_deck` (
-  `id` INTEGER PRIMARY KEY,
-  `card_id` int,
-  `deck_id` int
-);
-
-CREATE TABLE `cards_created_by_users` (
-  `id` INTEGER PRIMARY KEY,
-  `user_id` int,
-  `card_id` int
-);
+CREATE TABLE cards (
+  card_id INT PRIMARY KEY,
+  front VARCHAR,
+  back VARCHAR,
+  notes VARCHAR,
+  FOREIGN KEY (deck_id) INT
+    REFERENCES decks (deck_id)
+); 
