4263028a11167272abe28de18d7b1c759f67d3ec
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-07-21 20:33:15 +0900

    fixed formatting

--- wordsalad/db.py
+++ wordsalad/db.py
@@ -52,7 +52,9 @@ def init_db():
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
-    """Clear the existing data and create new tables."""
+    '''
+    Clear the existing data and create new tables
+    '''
     init_db()
     click.echo('Initialized the database.')
     
