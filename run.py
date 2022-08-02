2843f41a243d6dec1f8992ddcde5162876a85d88
J-Derrico <jama1091@colorado.edu>
2022-08-02 08:49:53 -0600

    updated run.py

--- run.py
+++ run.py
@@ -2,9 +2,10 @@ import os
 from unicodedata import name
 # from flask import Flask
 from wordsalad import create_app 
+from wordsalad import db
 
 app = create_app()
 
 if __name__== "__main_":
+    db.init_app(app) 
     app.run()
-
